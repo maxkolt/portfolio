584fb22ab532cd7ee6f7ff253db43d846b48b6f2
Максим Колтович <12345kolt@gmail.com>
2022-03-11 18:54:27 +0300

    разрабатывал компонент header

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+<Header/>
+    <v-main>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+
+import Header from "@/components/Header";
+export default {
+  name: 'App',
+  components: {Header},
+};
+</script>
